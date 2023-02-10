c34ef9ce6bb289deb1215bc719312f67c7c32939
LTMX <nicolasreinhard.pro@gmail.com>
2023-02-10 17:54:48 +0100

    Added documentation for Arithmetics

--- Runtime/Arithemetics.cs
+++ Runtime/Arithemetics.cs
@@ -10,154 +10,250 @@ namespace Unity.Mathematics
     [SuppressMessage("ReSharper", "IdentifierTypo")]
     public static partial class Math
     {
-        // Sign
+        /// Returns the sign of the given value.
         public static float4 sign(this float4 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static float3 sign(this float3 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static float2 sign(this float2 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static float sign(this float f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static int sign(this int f) => (int)math.sign(f);
         
+        ///<inheritdoc cref="sign(float4)"/>
         public static float4 sign(this Vector4 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static float3 sign(this Vector3 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static float2 sign(this Vector2 f) => math.sign(f);
         
+        ///<inheritdoc cref="sign(float4)"/>
         public static double4 sign(this double4 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static double3 sign(this double3 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static double2 sign(this double2 f) => math.sign(f);
+        ///<inheritdoc cref="sign(float4)"/>
         public static double sign(this double f) => math.sign(f);
         
         
         // Absolute Value ------------------------------------------------------------------------------
         
+        /// The componentwise absolute value of the input.
         public static float4 abs(this float4 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static float3 abs(this float3 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static float2 abs(this float2 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static float abs(this float f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static int abs(this int f) => math.abs(f);
         
+        /// <inheritdoc cref="abs(float4)"/>
         public static float4 abs(this Vector4 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static float3 abs(this Vector3 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static float2 abs(this Vector2 f) => math.abs(f);
         
+        /// <inheritdoc cref="abs(float4)"/>
         public static double4 abs(this double4 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static double3 abs(this double3 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static double2 abs(this double2 f) => math.abs(f);
+        /// <inheritdoc cref="abs(float4)"/>
         public static double abs(this double f) => math.abs(f);
 
 
         // Component-Wise Sum --------------------------------------------------------------------------
+        
+        /// Returns the sum of all components of the vector
         public static float sum(this float4 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static float sum(this float3 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static float sum(this float2 f) => math.csum(f);
         
+        /// <inheritdoc cref="sum(float4)"/> 
         public static float sum(this Vector4 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static float sum(this Vector3 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static float sum(this Vector2 f) => math.csum(f);
         
+        /// <inheritdoc cref="sum(float4)"/> 
         public static int sum(this int4 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static int sum(this int3 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static int sum(this int2 f) => math.csum(f);
         
+        /// <inheritdoc cref="sum(float4)"/> 
         public static double sum(this double4 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static double sum(this double3 f) => math.csum(f);
+        /// <inheritdoc cref="sum(float4)"/> 
         public static double sum(this double2 f) => math.csum(f);
 
         // Component-Wise Multiplication ---------------------------------------------------------------
-
+        
+        /// Returns the product of all components of the vector
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this float4 f) => f.x * f.y * f.z * f.w;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this float3 f) => f.x * f.y * f.z;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this float2 f) => f.x * f.y;
-
+        /// <inheritdoc cref="cmul(float4)"/>
+        
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this Vector4 f) => f.x * f.y * f.z * f.w;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this Vector3 f) => f.x * f.y * f.z;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static float cmul(this Vector2 f) => f.x * f.y;
         
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static int cmul(this int4 f) => f.x * f.y * f.z * f.w;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static int cmul(this int3 f) => f.x * f.y * f.z;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static int cmul(this int2 f) => f.x * f.y;
         
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static double cmul(this double4 f) => f.x * f.y * f.z * f.w;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static double cmul(this double3 f) => f.x * f.y * f.z;
+        /// <inheritdoc cref="cmul(float4)"/>
         [MethodImpl(AggressiveInlining)]
         public static double cmul(this double2 f) => f.x * f.y;
+        
 
 
         // One Minus -----------------------------------------------------------------------------------
 
+        /// Returns one minus the given value.
         public static float4 onem(this float4 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static float3 onem(this float3 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static float2 onem(this float2 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static float onem(this float f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static int onem(this int f) => 1 - f;
         
+        /// <inheritdoc cref="onem(float4)"/>
         public static double4 onem(this double4 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static double3 onem(this double3 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static double2 onem(this double2 f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         public static double onem(this double f) => 1 - f;
+        /// <inheritdoc cref="onem(float4)"/>
         
+        /// <inheritdoc cref="onem(float4)"/>
         public static float4 onem(this Vector4 f) => 1 - f.cast();
+        /// <inheritdoc cref="onem(float4)"/>
         public static float3 onem(this Vector3 f) => 1 - f.cast();
+        /// <inheritdoc cref="onem(float4)"/>
         public static float2 onem(this Vector2 f) => 1 - f.cast();
 
         // Negate -----------------------------------------------------------------------------------
 
+        /// Returns the negation of the given value.
         public static float4 neg(this float4 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static float3 neg(this float3 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static float2 neg(this float2 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static float neg(this float f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static int neg(this int f) => - f;
         
+        /// <inheritdoc cref="neg(float4)"/>
         public static double4 neg(this double4 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static double3 neg(this double3 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static double2 neg(this double2 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static double neg(this double f) => - f;
         
+        /// <inheritdoc cref="neg(float4)"/>
         public static float4 neg(this Vector4 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static float3 neg(this Vector3 f) => - f;
+        /// <inheritdoc cref="neg(float4)"/>
         public static float2 neg(this Vector2 f) => - f;
 
 
         // Reciprocal ----------------------------------------------------------------------------------
         
+        /// <summary>Returns the componentwise reciprocal a float4 vector.</summary>
         public static float4 rcp(this float4 f) => math.rcp(f);
+
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float3 rcp(this float3 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float2 rcp(this float2 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float rcp(this float f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float rcp(this int f) => math.rcp(f);
         
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float4 rcp(this Vector4 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float3 rcp(this Vector3 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static float2 rcp(this Vector2 f) => math.rcp(f);
         
+        /// <inheritdoc cref="rcp(float4)"/>
         public static double4 rcp(this double4 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static double3 rcp(this double3 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static double2 rcp(this double2 f) => math.rcp(f);
+        /// <inheritdoc cref="rcp(float4)"/>
         public static double rcp(this double f) => math.rcp(f);
 
 
 
         // Power ---------------------------------------------------------------------------------
 
+        /// <summary>Returns the componentwise result of raising x to the power y.</summary>
         public static float4 pow(this float4 f, float4 pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float3 pow(this float3 f, float3 pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float2 pow(this float2 f, float2 pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float pow(this float f, float pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float pow(this int f, float pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static int pow(this int f, int pow) => (int)math.pow(f, pow);
 
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float4 pow(this Vector4 f, float4 pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float3 pow(this Vector3 f, float3 pow) => math.pow(f, pow);
+        /// <inheritdoc cref="pow(float4, float4)"/>
         public static float2 pow(this Vector2 f, float2 pow) => math.pow(f, pow);
 
         public static double4 pow(this double4 f, double4 min) => math.pow(f, min);
@@ -168,24 +264,35 @@ namespace Unity.Mathematics
 
         // Square ---------------------------------------------------------------------------------
         
+        /// Returns x^2
         public static float4 sq(this float4 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static float3 sq(this float3 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static float2 sq(this float2 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static float sq(this float f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static int sq(this int f) => f * f;
         
+        /// <inheritdoc cref="sq(float4)"/>
         public static float4 sq(this Vector4 f) => f.cast() * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static float3 sq(this Vector3 f) => f.cast() * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static float2 sq(this Vector2 f) => f * f;
         
         public static double4 sq(this double4 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static double3 sq(this double3 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static double2 sq(this double2 f) => f * f;
+        /// <inheritdoc cref="sq(float4)"/>
         public static double sq(this double f) => f * f;
 
         // Cube --------------------------------------------------
         
-        /// <summary> returns x * x * x </summary>
+        /// <summary> returns x^3 </summary>
         public static float4 cube(this float4 f) => f * f * f;
         /// <inheritdoc cref="cube(float4)"/>
         public static float3 cube(this float3 f) => f * f * f;
