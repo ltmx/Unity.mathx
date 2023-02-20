d0042e66ffc425890012f6452efe7c99a26e344e
LTMX <47640688+LTMX@users.noreply.github.com>
2023-02-20 11:05:56 +0100

    Fixed Random.cs and added new functions for better implementation (resembling Random.Range() -- rand, randomInSphere, randomInCircle, varyrand, addrand, init

--- Runtime/Random.cs
+++ Runtime/Random.cs
@@ -4,36 +4,57 @@ namespace Unity.Mathematics
 {
     public partial class Math
     {
-        private static uint seed() => (uint) DateTime.Now.Millisecond;
-        private static Random random => new Random(seed()).Init();
-
-        // Extension returning type
-        public static Random Init(this Random random){
-            random.InitState();
-            return random;
+        // private static uint seed() => (uint) DateTime.Now.Millisecond;
+        private static Random r = new(0x6E624EB7u);
+        
+        public static Random init(this Random rand){
+            rand.InitState();
+            return rand;
         }
+        
+        public static float rand() => r.NextFloat();
+        public static float rand(this float max) => r.NextFloat(max);
+        
+        public static float varyrand(this float a, float min, float max) => a + rand(min, max);
+        public static float2 varyrand(this float2 a, float2 min, float2 max) => a + rand(min, max);
+        public static float3 varyrand(this float3 a, float3 min, float3 max) => a + rand(min, max);
+        public static float4 varyrand(this float4 a, float4 min, float4 max) => a + rand(min, max);
+        
+        public static float addrand(this float a, float max) => a + r.NextFloat(max);
+        public static float2 addrand(this float2 a, float2 max) => a + r.NextFloat2(max);
+        public static float3 addrand(this float3 a, float3 max) => a + r.NextFloat3(max);
+        public static float4 addrand(this float4 a, float4 max) => a + r.NextFloat4(max);
+        
+        public static float2 addrand(this float2 a, float max) => a + r.NextFloat2(max);
+        public static float3 addrand(this float3 a, float max) => a + r.NextFloat3(max);
+        public static float4 addrand(this float4 a, float max) => a + r.NextFloat4(max);
 
-        public static float randomrange(float min, float max) => random.NextFloat(min, max);
-        public static float2 randomrange(float2 min, float2 max) => random.NextFloat2(min, max);
-        public static float3 randomrange(float3 min, float3 max) => random.NextFloat3(min, max);
-        public static float4 randomrange(float4 min, float4 max) => random.NextFloat4(min, max);
-        
-        public static float randomintrange(int min, int max) => random.NextInt(min, max);
-        public static int2 randomint2range(int min, int max) => random.NextInt2(min, max);
-        public static int3 randomint3range(int min, int max) => random.NextInt3(min, max);
-        public static int4 randomint4range(int min, int max) => random.NextInt4(min, max);
-        
-        public static float3 randomInsideUnitSphere() => random.NextFloat3Direction() * random.NextFloat().cube();
-        public static float2 randomInsideUnitCircle() => random.NextFloat2Direction() * random.NextFloat().sq();
-        
-        public static float3 randomInsideSphere(float radius = 1) => random.NextFloat3Direction() * random.NextFloat().cube() * radius; // Always outputting the same value
-        public static float2 randomInsideCircle(float radius = 1) => random.NextFloat2Direction() * random.NextFloat().sq() * radius;
-        
-        public static float3 random3Ddirection() => random.NextFloat3Direction();
-        public static float2 random2Ddirection() => random.NextFloat2Direction();
-        
-        public static quaternion randomquaternionrotation() => random.NextQuaternionRotation();
         
 
+        
+        public static float rand(float min, float max) => r.NextFloat(min, max);
+        public static float2 rand(float2 min, float2 max) => r.NextFloat2(min, max);
+        public static float3 rand(float3 min, float3 max) => r.NextFloat3(min, max);
+        public static float4 rand(float4 min, float4 max) => r.NextFloat4(min, max);
+        
+        public static float2 rand(float min, float2 max) => r.NextFloat2(min, max);
+        public static float3 rand(float min, float3 max) => r.NextFloat3(min, max);
+        public static float4 rand(float min, float4 max) => r.NextFloat4(min, max);
+        public static float2 rand(float2 min, float max) => r.NextFloat2(min, max);
+        public static float3 rand(float3 min, float max) => r.NextFloat3(min, max);
+        public static float4 rand(float4 min, float max) => r.NextFloat4(min, max);
+        
+        public static int randomint(int min, int max) => r.NextInt(min, max);
+        public static int2 randomint(int2 min, int2 max) => r.NextInt2(min, max); 
+        public static int3 randomint(int3 min, int3 max) => r.NextInt3(min, max);
+        public static int4 randomint(int4 min, int4 max) => r.NextInt4(min, max);
+
+        public static float3 randomInSphere(float radius = 1) => r.NextFloat3Direction() * r.NextFloat().cube() * radius; // Always outputting the same value
+        public static float2 randomInCircle(float radius = 1) => r.NextFloat2Direction() * r.NextFloat().sq() * radius;
+        
+        public static float3 randomDir3D() => r.NextFloat3Direction();
+        public static float2 randomDir2D() => r.NextFloat2Direction();
+        
+        public static quaternion randomrotation() => r.NextQuaternionRotation();
     }
 }
