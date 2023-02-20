f1a3e3c47cd7f3d82eb2d58db7975545b5aa2648
LTMX <47640688+LTMX@users.noreply.github.com>
2023-02-20 11:06:08 +0100

    Added Test.cs

---
+++ Runtime/Test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using static Unity.Mathematics.Math;
+
+public class Test : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rand(0,1));
+    }
+}
