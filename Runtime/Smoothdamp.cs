a3b26ce2a455728b3dc51efcfaeafc5c04c3866d
LTMX <47640688+LTMX@users.noreply.github.com>
2023-02-20 17:26:42 +0100

    Implemented better smoothdamp function

--- Runtime/Smoothdamp.cs
+++ Runtime/Smoothdamp.cs
@@ -23,130 +23,115 @@ namespace Unity.Mathematics
         public static float smoothdamp(float current, float target, ref float currentVelocity, float smoothTime, float maxSpeed = float.PositiveInfinity) {
             return smoothdamp(current, target, ref currentVelocity, smoothTime, maxSpeed, Time.deltaTime);
         }
+        
 
-
-        // From Game Programming Gems 4 Chapter 1.10
-        public static float4 smoothdamp(float4 current, float4 target, ref float4 currentVelocity, float smoothTime,
-            [System.ComponentModel.DefaultValue("Mathf.Infinity")] float maxSpeed, [System.ComponentModel.DefaultValue("Time.deltaTime")] float deltaTime)
+        public static float smoothdamp(float current, float target, ref float velocity, float smoothTime, 
+            [System.ComponentModel.DefaultValue("float.PositiveInfinity")]float maxSpeed, 
+            [System.ComponentModel.DefaultValue("Time.deltaTime")]float deltaTime)
         {
-            smoothTime = smoothTime.max(0.0001f);
-            var omega = 2 / smoothTime;
+            smoothTime = 0.0001f.max(smoothTime);
+            float omega = 2 / smoothTime;
 
-            var x = omega * deltaTime;
-            var exp = (x * (1 + x * (0.48f + x * 0.235f))).rcp();
+            float x = omega * deltaTime;
+            float exp = (x * (x * (0.48f + x * 0.235f) + 1 )).rcp();
+
             var change = current - target;
-            var originalTo = target;
-
-            // Clamp maximum speed
             var maxChange = maxSpeed * smoothTime;
             change = change.clamp(-maxChange, maxChange);
-            // var num9 = change.sqr().sum();
-            // if (num9 > maxChange.sqr()) change /= num9.sqrt() * maxChange;
-            target = current - change;
 
-            var temp = (currentVelocity + omega * change) * deltaTime;
-            currentVelocity = (currentVelocity - omega * temp) * exp;
-            var output = target + (change + temp) * exp;
+            var temp = (velocity + omega * change) * deltaTime;
+            velocity = (velocity - omega * temp) * exp;
 
-            var num14 = originalTo - current.x;
-            var num17 = output - originalTo;
+            var result = current - change + (change + temp) * exp;
 
-            if ((num14 * num17).sum() <= 0) return output;
+            if ((target - current) * (result - target) <= 0) return result;
+            
+            result = target;
+            velocity = 0;
 
-            output = originalTo;
-            currentVelocity = (output - originalTo.x) / deltaTime;
-            return output;
+            return result;
         }
 
-        public static float smoothdamp(float current, float target, ref float currentVelocity, float smoothTime,
-            [UnityEngine.Internal.DefaultValueAttribute("Mathf.Infinity")]
-            float maxSpeed, [UnityEngine.Internal.DefaultValueAttribute("Time.deltaTime")]
-            float deltaTime)
+        public static float2 smoothdamp(float2 current, float2 target, ref float2 velocity, float smoothTime, 
+            [System.ComponentModel.DefaultValue("float.PositiveInfinity")]float maxSpeed, 
+            [System.ComponentModel.DefaultValue("Time.deltaTime")]float deltaTime)
         {
-            smoothTime = smoothTime.max(0.0001f);
-            var omega = 2 / smoothTime;
+            smoothTime = 0.0001f.max(smoothTime);
+            float omega = 2 / smoothTime;
 
-            var x = omega * deltaTime;
-            var exp = (x * (1 + x * (0.48f + x * 0.235f))).rcp();
+            float x = omega * deltaTime;
+            float exp = (x * (x * (0.48f + x * 0.235f) + 1 )).rcp();
+
             var change = current - target;
-            var originalTo = target;
-
-            // Clamp maximum speed
             var maxChange = maxSpeed * smoothTime;
             change = change.clamp(-maxChange, maxChange);
-            target = current - change;
 
-            var temp = (currentVelocity + omega * change) * deltaTime;
-            currentVelocity = (currentVelocity - omega * temp) * exp;
-            var output = target + (change + temp) * exp;
+            var temp = (velocity + omega * change) * deltaTime;
+            velocity = (velocity - omega * temp) * exp;
 
-            // Prevent overshooting
-            if (originalTo - current > 0 != output > originalTo) return output;
-            output = originalTo;
-            currentVelocity = (output - originalTo) / deltaTime;
+            var result = current - change + (change + temp) * exp;
 
-            return output;
+            if ((target - current).dot(result - target) <= 0) return result;
+            
+            result = target;
+            velocity = 0;
+
+            return result;
         }
 
-
-        public static float3 smoothdamp(float3 current, float3 target, ref float3 currentVelocity, float smoothTime,
-            [System.ComponentModel.DefaultValue("Mathf.Infinity")] float maxSpeed, [System.ComponentModel.DefaultValue("Time.deltaTime")] float deltaTime)
+        public static float3 smoothdamp(float3 current, float3 target, ref float3 velocity, float smoothTime, 
+            [System.ComponentModel.DefaultValue("float.PositiveInfinity")]float maxSpeed, 
+            [System.ComponentModel.DefaultValue("Time.deltaTime")]float deltaTime)
         {
-            smoothTime = smoothTime.max(0.0001f);
-            var omega = 2 / smoothTime;
+            smoothTime = 0.0001f.max(smoothTime);
+            float omega = 2 / smoothTime;
 
-            var x = omega * deltaTime;
-            var exp = (x * (1 + x * (0.48f + x * 0.235f))).rcp();
+            float x = omega * deltaTime;
+            float exp = (x * (x * (0.48f + x * 0.235f) + 1 )).rcp();
+
             var change = current - target;
-            var originalTo = target;
-
-            // Clamp maximum speed
             var maxChange = maxSpeed * smoothTime;
             change = change.clamp(-maxChange, maxChange);
-            target = current - change;
 
-            var temp = (currentVelocity + omega * change) * deltaTime;
-            currentVelocity = (currentVelocity - omega * temp) * exp;
-            var output = target + (change + temp) * exp;
+            var temp = (velocity + omega * change) * deltaTime;
+            velocity = (velocity - omega * temp) * exp;
 
-            var num14 = originalTo - current.x;
-            var num17 = output - originalTo;
+            var result = current - change + (change + temp) * exp;
 
-            if ((num14 * num17).sum() <= 0) return output;
+            if ((target - current).dot(result - target) <= 0) return result;
+            
+            result = target;
+            velocity = 0;
 
-            output = originalTo;
-            currentVelocity = (output - originalTo.x) / deltaTime;
-            return output;
+            return result;
         }
-
-        public static float2 smoothdamp(float2 current, float2 target, ref float2 currentVelocity, float smoothTime,
-            [System.ComponentModel.DefaultValue("Mathf.Infinity")] float maxSpeed, [System.ComponentModel.DefaultValue("Time.deltaTime")] float deltaTime)
+        
+        public static float4 smoothdamp(float4 current, float4 target, ref float4 velocity, float smoothTime, 
+            [System.ComponentModel.DefaultValue("float.PositiveInfinity")]float maxSpeed, 
+            [System.ComponentModel.DefaultValue("Time.deltaTime")]float deltaTime)
         {
-            smoothTime = smoothTime.max(0.0001f);
-            var omega = 2 / smoothTime;
+            smoothTime = 0.0001f.max(smoothTime);
+            float omega = 2 / smoothTime;
 
-            var x = omega * deltaTime;
-            var exp = (x * (1 + x * (0.48f + x * 0.235f))).rcp();
+            float x = omega * deltaTime;
+            float exp = (x * (x * (0.48f + x * 0.235f) + 1 )).rcp();
+
             var change = current - target;
-            var originalTo = target;
-
-            // Clamp maximum speed
             var maxChange = maxSpeed * smoothTime;
             change = change.clamp(-maxChange, maxChange);
-            target = current - change;
 
-            var temp = (currentVelocity + omega * change) * deltaTime;
-            currentVelocity = (currentVelocity - omega * temp) * exp;
-            var output = target + (change + temp) * exp;
+            var temp = (velocity + omega * change) * deltaTime;
+            velocity = (velocity - omega * temp) * exp;
 
-            var num14 = originalTo - current.x;
-            var num17 = output - originalTo;
+            var result = current - change + (change + temp) * exp;
 
-            if ((num14 * num17).sum() <= 0) return output;
+            if ((target - current).dot(result - target) <= 0) return result;
+            
+            result = target;
+            velocity = 0;
 
-            output = originalTo;
-            currentVelocity = (output - originalTo.x) / deltaTime;
-            return output;
+            return result;
         }
+
     }
 }
