6793e77e51812acd6e12319b1ebb175cae546e37
LTMX <47640688+LTMX@users.noreply.github.com>
2023-03-18 15:58:52 +0100

    Added overloads for asbool and asuint

--- Runtime/Conversion.Common.cs
+++ Runtime/Conversion.Common.cs
@@ -43,6 +43,8 @@ namespace Unity.Mathematics
         [MethodImpl(IL)] public static int2 asint(this double2 f) => (int2) f;
         /// <inheritdoc cref="asint(float4)"/>>
         [MethodImpl(IL)] public static int asint(this double f) => (int) f;
+        
+        
 
         /// Return an equivalent bool-type
         [MethodImpl(IL)] public static bool4 asbool(this int4 f) => f != 0;
@@ -98,6 +100,50 @@ namespace Unity.Mathematics
         [MethodImpl(IL)] public static int asint(this bool b) => b ? 1 : 0;
         
         
+        // bools as uints -------------------------------------------
+        /// Returns 1 when true, zero otherwise componentwise
+        [MethodImpl(IL)] public static uint4 asuint(this bool4 b) => (uint4) b;
+        /// <inheritdoc cref="asuint(bool4)"/>
+        [MethodImpl(IL)] public static uint3 asuint(this bool3 b) => (uint3) b;
+        /// <inheritdoc cref="asuint(bool4)"/>
+        [MethodImpl(IL)] public static uint2 asuint(this bool2 b) => (uint2) b;
+        [MethodImpl(IL)] public static uint asuint(this bool b) => b ? 1u : 0u;
+        
+        // ints as uints -------------------------------------------
+        [MethodImpl(IL)] public static uint4 asuint(this int4 b) => (uint4)b;
+        [MethodImpl(IL)] public static uint3 asuint(this int3 b) => (uint3)b;
+        [MethodImpl(IL)] public static uint2 asuint(this int2 b) => (uint2)b;
+        [MethodImpl(IL)] public static uint asuint(this int b) => (uint)b;
+        
+        // floats as uints -------------------------------------------
+        /// Returns 1 when true, zero otherwise componentwise
+        [MethodImpl(IL)] public static uint4 asuint(this float4 b) => (uint4)b;
+        /// <inheritdoc cref="asuint(float4)"/>
+        [MethodImpl(IL)] public static uint3 asuint(this float3 b) => (uint3)b;
+        /// <inheritdoc cref="asuint(float4)"/>
+        [MethodImpl(IL)] public static uint2 asuint(this float2 b) => (uint2)b;
+        [MethodImpl(IL)] public static uint asuint(this float b) => (uint)b;
+        
+        // uints as ints -------------------------------------------
+        /// Cast to int4
+        [MethodImpl(IL)] public static int4 asint(this uint4 b) => (int4)b;
+        /// Cast to int
+        [MethodImpl(IL)] public static int3 asint(this uint3 b) => (int3)b;
+        /// Cast to int2
+        [MethodImpl(IL)] public static int2 asint(this uint2 b) => (int2)b;
+        /// Cast to int
+        [MethodImpl(IL)] public static int asint(this uint b) => (int)b;
+        
+        // uints as floats -------------------------------------------
+        /// Returns 1 when true, zero otherwise componentwise
+        [MethodImpl(IL)] public static bool4 asbool(this uint4 b) => b != 0;
+        /// <inheritdoc cref="asuint(bool4)"/>
+        [MethodImpl(IL)] public static bool3 asbool(this uint3 b) => b != 0;
+        /// <inheritdoc cref="asuint(bool4)"/>
+        [MethodImpl(IL)] public static bool2 asbool(this uint2 b) => b != 0;
+        /// Returns true when not zero
+        [MethodImpl(IL)] public static bool asbool(this uint b) => b != 0;
+        
         // bools as floats -------------------------------------------
         
         /// Returns 1 when true, componentwise
@@ -107,7 +153,7 @@ namespace Unity.Mathematics
         /// <inheritdoc cref="asfloat(bool4)"/>
         [MethodImpl(IL)] public static float2 asfloat(this bool2 b) => (float2) b;
         /// Returns 1 when true
-        [MethodImpl(IL)] public static float asfloat(this bool b) => b.asint();
+        [MethodImpl(IL)] public static float asfloat(this bool b) => b ? 1f : 0f;
         
         // doubles as floats -------------------------------------------
         
