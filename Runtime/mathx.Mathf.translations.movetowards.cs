e06f26a3596c524abe7d0deb7a1e2647cd054612
Martônio Júnior <martoniojunior@hotmail.com>
2023-04-23 16:26:33 -0300

    Simplified formula for movetowards

--- Runtime/mathx.Mathf.translations.movetowards.cs
+++ Runtime/mathx.Mathf.translations.movetowards.cs
@@ -11,69 +11,64 @@ namespace Unity.Mathematics
     public partial class mathx
     {
         /// Calculate a position between the points specified by current and target, moving no farther than the distance specified by maxDistanceDelta
+        public static float4 movetowards(this float4 current, float4 target, float4 maxDistanceDelta)
+        {
+            var delta = target - current;
+            return math.mad(min(abs(delta),maxDistanceDelta),sign(delta),current);
+        }
+        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float4)"/>
         public static float4 movetowards(this float4 current, float4 target, float maxDistanceDelta)
         {
             var delta = target - current;
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current + delta) / deltaLength.sqrt() * maxDistanceDelta;
+            var deltaLength = delta.length();
+            return math.mad(min(deltaLength,maxDistanceDelta),sign(delta),current);
+        }
+        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
+        public static float4 movetowards(this Vector4 current, float4 target, float maxDistanceDelta)
+        {
+            return movetowards(current.asfloat(),target,maxDistanceDelta);
+        }
+        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
+        public static float3 movetowards(this float3 current, float3 target, float3 maxDistanceDelta)
+        {
+            var delta = target - current;
+            return math.mad(min(abs(delta),maxDistanceDelta),sign(delta),current);
         }
         /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
         public static float3 movetowards(this float3 current, float3 target, float maxDistanceDelta)
         {
             var delta = target - current;
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current + delta) / deltaLength.sqrt() * maxDistanceDelta;
+            var deltaLength = delta.length();
+            return math.mad(min(deltaLength,maxDistanceDelta),sign(delta),current);
+        }
+        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
+        public static float3 movetowards(this Vector3 current, float3 target, float maxDistanceDelta)
+        {
+            return movetowards(current.asfloat(), target, maxDistanceDelta);
+        }
+        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
+        public static float2 movetowards(this float2 current, float2 target, float2 maxDistanceDelta)
+        {
+            var delta = target - current;
+            return math.mad(min(abs(delta),maxDistanceDelta),sign(delta),current);
         }
         /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
         public static float2 movetowards(this float2 current, float2 target, float maxDistanceDelta)
         {
             var delta = target - current;
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current + delta) / deltaLength.sqrt() * maxDistanceDelta;
+            var deltaLength = delta.length();
+            return math.mad(min(deltaLength,maxDistanceDelta),sign(delta),current);
         }
         /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
         public static float movetowards(this float current, float target, float maxDistanceDelta)
         {
             var delta = target - current;
-            var deltaLength = delta.sq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current + delta) / deltaLength.sqrt() * maxDistanceDelta;
-        }
-        
-        
-        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
-        public static float4 movetowards(this Vector4 current, float4 target, float maxDistanceDelta)
-        {
-            var delta = target - current.asfloat();
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current.asfloat() + delta) / deltaLength.sqrt() * maxDistanceDelta;
-        }
-        /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
-        public static float3 movetowards(this Vector3 current, float3 target, float maxDistanceDelta)
-        {
-            var delta = target - current.asfloat();
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current.asfloat() + delta) / deltaLength.sqrt() * maxDistanceDelta;
+            return math.mad(min(abs(delta),maxDistanceDelta),sign(delta),current);
         }
         /// <inheritdoc cref="movetowards(Mathematics.float4,Mathematics.float4,float)"/>
         public static float2 movetowards(this Vector2 current, float2 target, float maxDistanceDelta)
         {
-            var delta = target - current.asfloat();
-            var deltaLength = delta.lengthsq();
-            if (deltaLength == 0 || maxDistanceDelta >= 0 && deltaLength <= maxDistanceDelta.sq())
-                return target;
-            return (current.asfloat() + delta) / deltaLength.sqrt() * maxDistanceDelta;
+            return movetowards(current.asfloat(), target, maxDistanceDelta);
         }
     }
 }
