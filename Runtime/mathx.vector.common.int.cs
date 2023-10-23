9bf34092de6abe3948f375d2cd31b0e42740565e
LTMX <47640688+LTMX@users.noreply.github.com>
2023-10-23 10:57:27 +0100

    removed need for casts in some methods, and unified naming convensions

--- Runtime/mathx.vector.common.int.cs
+++ Runtime/mathx.vector.common.int.cs
@@ -12,56 +12,73 @@ namespace Unity.Mathematics
     {
 
         /// <inheritdoc cref="math.normalize(float4)"/>
-        [MethodImpl(IL)] public static float4 norm(this int4 f) => math.normalize(f);
+        [MethodImpl(IL)] public static float4 norm(this int4 x) => math.normalize(x);
         /// <inheritdoc cref="math.normalize(float3)"/>
-        [MethodImpl(IL)] public static float3 norm(this int3 f) => math.normalize(f);
+        [MethodImpl(IL)] public static float3 norm(this int3 x) => math.normalize(x);
         /// <inheritdoc cref="math.normalize(float2)"/>
-        [MethodImpl(IL)] public static float2 norm(this int2 f) => math.normalize(f);
+        [MethodImpl(IL)] public static float2 norm(this int2 x) => math.normalize(x);
 
-        /// <inheritdoc cref="math.normalizesafe(float2,float2)"/>
-        [MethodImpl(IL)] public static float4 normsafe(this int4 f, int4 d = default) => math.normalizesafe(f, d);
-        /// <inheritdoc cref="math.normalizesafe(int3,int3)"/>
-        [MethodImpl(IL)] public static float3 normsafe(this int3 f, int3 d = default) => math.normalizesafe(f, d);
-        /// <inheritdoc cref="math.normalizesafe(float2,float2)"/>
-        [MethodImpl(IL)] public static float2 normsafe(this int2 f, int2 d = default) => math.normalizesafe(f, d);
+        /// <returns>The normalized vector or the default value if the normalized vector is not finite.</returns>
+        [MethodImpl(IL)]         
+        public static float4 normsafe(int4 x, float4 defaultvalue = default)
+        {
+            float len = math.dot(x, x);
+            return math.select(defaultvalue, x.mult(math.rsqrt(len)), len > math.FLT_MIN_NORMAL);
+        }
+        /// <inheritdoc cref="normsafe(int4, float4)"/>
+        [MethodImpl(IL)]
+        public static float3 normsafe(int3 x, float3 defaultvalue = default)
+        {
+            float len = math.dot(x, x);
+            return math.select(defaultvalue, x.mult(math.rsqrt(len)), len > math.FLT_MIN_NORMAL);
+        }
+        /// <inheritdoc cref="normsafe(int4, float4)"/>
+        [MethodImpl(IL)]         
+        public static float2 normsafe(int2 x, float2 defaultvalue = default) 
+        {
+            float len = math.dot(x, x);
+            return math.select(defaultvalue, x.mult(math.rsqrt(len)), len > math.FLT_MIN_NORMAL);
+        }
         
-        /// <inheritdoc cref="math.distance(int2,int2)"/>
-        [MethodImpl(IL)] public static float distance(this int2 f, int2 f2) => math.distance(f, f2);
-        /// <inheritdoc cref="math.distance(int3,int3)"/>
-        [MethodImpl(IL)] public static float distance(this int3 f, int3 f2) => math.distance(f, f2);
-        /// <inheritdoc cref="math.distance(int4,int4)"/>
-        [MethodImpl(IL)] public static float distance(this int4 f, int4 f2) => math.distance(f, f2);
+        /// <returns>The distance between x and y</returns>
+        [MethodImpl(IL)] public static float distance(this int4 x, int4 y) => length(y - x);
+        /// <inheritdoc cref="distance(int4,int4)"/>
+        [MethodImpl(IL)] public static float distance(this int3 x, int3 y) => length(y - x);
+        /// <inheritdoc cref="distance(int4,int4)"/>
+        [MethodImpl(IL)] public static float distance(this int2 x, int2 y) => length(y - x);
         
-        /// <inheritdoc cref="math.distancesq(int2,int2)"/>
-        [MethodImpl(IL)] public static float distancesq(this int2 f, int2 f2) => math.distancesq(f, f2);
-        /// <inheritdoc cref="math.distancesq(int3,int3)"/>
-        [MethodImpl(IL)] public static float distancesq(this int3 f, int3 f2) => math.distancesq(f, f2);
-        /// <inheritdoc cref="math.distancesq(int4,int4)"/>
-        [MethodImpl(IL)] public static float distancesq(this int4 f, int4 f2) => math.distancesq(f, f2);
+        /// <returns>The squared distance between x and y.</returns>
+        [MethodImpl(IL)] public static float distancesq(this int4 x, int4 y) => lengthsq(y - x);
+        /// <inheritdoc cref="distancesq(int4,int4)"/>
+        [MethodImpl(IL)] public static float distancesq(this int3 x, int3 y) => lengthsq(y - x);
+        /// <inheritdoc cref="distancesq(int4,int4)"/>
+        [MethodImpl(IL)] public static float distancesq(this int2 x, int2 y) => lengthsq(y - x);
+
+
 
         
-        /// <inheritdoc cref="math.length(int4)"/>
-        [MethodImpl(IL)] public static float length(this int4 f) => math.length(f);
-        /// <inheritdoc cref="math.length(int3)"/>
-        [MethodImpl(IL)] public static float length(this int3 f) => math.length(f);
-        /// <inheritdoc cref="math.length(int2)"/>
-        [MethodImpl(IL)] public static float length(this int2 f) => math.length(f);
+        /// <returns>Length of x</returns>
+        [MethodImpl(IL)] public static float length(this int4 x) => math.sqrt(math.dot(x, x));
+        /// <returns>Length of x</returns>
+        [MethodImpl(IL)] public static float length(this int3 x) => math.sqrt(math.dot(x, x));
+        /// <returns>Length of x</returns>
+        [MethodImpl(IL)] public static float length(this int2 x) => math.sqrt(math.dot(x, x));
         
 
-        /// <inheritdoc cref="math.lengthsq(int4)"/>
-        [MethodImpl(IL)] public static float lengthsq(this int4 f) => math.lengthsq(f);
-        /// <inheritdoc cref="math.lengthsq(int3)"/>
-        [MethodImpl(IL)] public static float lengthsq(this int3 f) => math.lengthsq(f);
-        /// <inheritdoc cref="math.lengthsq(int2)"/>
-        [MethodImpl(IL)] public static float lengthsq(this int2 f) => math.lengthsq(f);
+        /// <returns>Squared length of x</returns>
+        [MethodImpl(IL)] public static float lengthsq(this int4 x) => math.dot(x, x); // using the original implementation removes an unnecessary cast
+        /// <returns>Squared length of x</returns>
+        [MethodImpl(IL)] public static float lengthsq(this int3 x) => math.dot(x, x);
+        /// <returns>Squared length of x</returns>
+        [MethodImpl(IL)] public static float lengthsq(this int2 x) => math.dot(x, x);
 
 
-        /// <inheritdoc cref="dot(int4,int4)"/>
-        [MethodImpl(IL)] public static int dot(this int4 f, int4 f2) => math.dot(f, f2);
-        /// <inheritdoc cref="dot(int4,int4)"/>
-        [MethodImpl(IL)] public static int dot(this int3 f, int3 f2) => math.dot(f, f2);
-        /// <inheritdoc cref="dot(int4,int4)"/>
-        [MethodImpl(IL)] public static int dot(this int2 f, int2 f2) => math.dot(f, f2);
+        /// <inheritdoc cref="math.dot(int4,int4)"/>
+        [MethodImpl(IL)] public static int dot(this int4 x, int4 y) => math.dot(x, y);
+        /// <inheritdoc cref="math.dot(int4,int4)"/>
+        [MethodImpl(IL)] public static int dot(this int3 x, int3 y) => math.dot(x, y);
+        /// <inheritdoc cref="math.dot(int4,int4)"/>
+        [MethodImpl(IL)] public static int dot(this int2 x, int2 y) => math.dot(x, y);
         
 
         /// Given an incident vector i and a normal vector n, returns the reflection vector r = i - 2.0f * dot(i, n) * n.
@@ -73,25 +90,25 @@ namespace Unity.Mathematics
         
         
         /// Returns the refraction vector given the incident vector i, the normal vector n and the refraction index eta.
-        [MethodImpl(IL)] public static float2 refract(this int2 f, int2 f2, float eta) => math.refract(f, f2, eta);
+        [MethodImpl(IL)] public static float2 refract(this int2 x, int2 f2, float eta) => math.refract(x, f2, eta);
         /// <inheritdoc cref="refract(int2,int2,float)"/>
-        [MethodImpl(IL)] public static float3 refract(this int3 f, int3 f2, float eta) => math.refract(f, f2, eta);
+        [MethodImpl(IL)] public static float3 refract(this int3 x, int3 f2, float eta) => math.refract(x, f2, eta);
         /// <inheritdoc cref="refract(int2,int2,float)"/>
-        [MethodImpl(IL)] public static float4 refract(this int4 f, int4 f2, float eta) => math.refract(f, f2, eta);
+        [MethodImpl(IL)] public static float4 refract(this int4 x, int4 f2, float eta) => math.refract(x, f2, eta);
 
         /// <inheritdoc cref="math.project(float4,float4)"/>
-        [MethodImpl(IL)] public static float4 project(this int4 f, int4 n) => math.project(f, n);
+        [MethodImpl(IL)] public static float4 project(this int4 x, int4 n) => math.project(x, n);
         /// <inheritdoc cref="math.project(float3,float3)"/>
-        [MethodImpl(IL)] public static float3 project(this int3 f, int3 n) => math.project(f, n);
+        [MethodImpl(IL)] public static float3 project(this int3 x, int3 n) => math.project(x, n);
         /// <inheritdoc cref="math.project(float2,float2)"/>
-        [MethodImpl(IL)] public static float2 project(this int2 f, int2 n) => math.project(f, n);
+        [MethodImpl(IL)] public static float2 project(this int2 x, int2 n) => math.project(x, n);
         
         /// <inheritdoc cref="math.projectsafe(float4,float4,float4)"/>
-        [MethodImpl(IL)] public static float4 projectsafe(this int4 f, float4 n, float4 defaultValue = default) => math.projectsafe(f, n, defaultValue);
+        [MethodImpl(IL)] public static float4 projectsafe(this int4 x, float4 n, float4 defaultValue = default) => math.projectsafe(x, n, defaultValue);
         /// <inheritdoc cref="math.projectsafe(float2,float2,float2)"/>
-        [MethodImpl(IL)] public static float3 projectsafe(this int3 f, float3 n, float3 defaultValue = default) => math.projectsafe(f, n, defaultValue);
+        [MethodImpl(IL)] public static float3 projectsafe(this int3 x, float3 n, float3 defaultValue = default) => math.projectsafe(x, n, defaultValue);
         /// <inheritdoc cref="math.projectsafe(float2,float2,float2)"/>
-        [MethodImpl(IL)] public static float2 projectsafe(this int2 f, float2 n, float2 defaultValue = default) => math.projectsafe(f, n, defaultValue);
+        [MethodImpl(IL)] public static float2 projectsafe(this int2 x, float2 n, float2 defaultValue = default) => math.projectsafe(x, n, defaultValue);
         
         
         /// Returns the Manhattan distance between two float4a
@@ -110,8 +127,8 @@ namespace Unity.Mathematics
         ///<inheritdoc cref="math.cross(float3,float3)"/>
         [MethodImpl(IL)] public static int3 cross(this int3 x, int3 y) => (x * y.yzx - x.yzx * y).yzx;
         
-        /// returns a vector perpendicular to the input vector
-        [MethodImpl(IL)] public static int2 perp(this int2 f) => new(-f.y, f.x);
+        /// returns a vector perpendicular to x
+        [MethodImpl(IL)] public static int2 perp(this int2 x) => new(-x.y, x.x);
         
         /// Returns the exterior product of two vectors. its magnitude is the area of the parallelogram they span.
         /// note: this is not the cross product
@@ -125,25 +142,25 @@ namespace Unity.Mathematics
         #region Matrix Operations
 
         ///Returns the distance the two vectors in the matrix
-        [MethodImpl(IL)] public static float cdistance(this int2x2 f) => math.distance(f.c0, f.c1);
-        [MethodImpl(IL)] public static float cdistance(this int3x2 f) => math.distance(f.c0, f.c1);
-        [MethodImpl(IL)] public static float cdistance(this int4x2 f) => math.distance(f.c0, f.c1);
+        [MethodImpl(IL)] public static float cdistance(this int2x2 x) => math.distance(x.c0, x.c1);
+        [MethodImpl(IL)] public static float cdistance(this int3x2 x) => math.distance(x.c0, x.c1);
+        [MethodImpl(IL)] public static float cdistance(this int4x2 x) => math.distance(x.c0, x.c1);
         
         ///Returns the squared distance the two vectors in the matrix
-        [MethodImpl(IL)] public static float cdistancesq(this int2x2 f) => math.distancesq(f.c0, f.c1);
-        [MethodImpl(IL)] public static float cdistancesq(this int3x2 f) => math.distancesq(f.c0, f.c1);
-        [MethodImpl(IL)] public static float cdistancesq(this int4x2 f) => math.distancesq(f.c0, f.c1);
+        [MethodImpl(IL)] public static float cdistancesq(this int2x2 x) => math.distancesq(x.c0, x.c1);
+        [MethodImpl(IL)] public static float cdistancesq(this int3x2 x) => math.distancesq(x.c0, x.c1);
+        [MethodImpl(IL)] public static float cdistancesq(this int4x2 x) => math.distancesq(x.c0, x.c1);
         
         
         ///Returns the cross product of the two vectors in the matrix
-        [MethodImpl(IL)] public static float3 ccross(this int3x2 f) => math.cross(f.c0, f.c1);
+        [MethodImpl(IL)] public static float3 ccross(this int3x2 x) => math.cross(x.c0, x.c1);
         
         ///Returns the dot product of the two vectors in the matrix
-        [MethodImpl(IL)] public static int cdot(this int4x2 f) => math.dot(f.c0, f.c1);
+        [MethodImpl(IL)] public static int cdot(this int4x2 x) => math.dot(x.c0, x.c1);
         /// <inheritdoc cref="cdot(int4x2)"/>
-        [MethodImpl(IL)] public static int cdot(this int3x2 f) => math.dot(f.c0, f.c1);
+        [MethodImpl(IL)] public static int cdot(this int3x2 x) => math.dot(x.c0, x.c1);
         /// <inheritdoc cref="cdot(int4x2)"/>
-        [MethodImpl(IL)] public static int cdot(this int2x2 f) => math.dot(f.c0, f.c1);
+        [MethodImpl(IL)] public static int cdot(this int2x2 x) => math.dot(x.c0, x.c1);
 
         #endregion
     
