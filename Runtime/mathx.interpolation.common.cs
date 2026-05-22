86d66c4a4d15289f4c9985e390e651209a90ceef
Nicolas Reinhard <nicolasreinhard.pro@gmail.com>
2026-05-22 19:46:23 +0200

    made type explicit in smax_exp

--- Runtime/mathx.interpolation.common.cs
+++ Runtime/mathx.interpolation.common.cs
@@ -290,7 +290,7 @@ namespace Unity.Mathematics
         
  
         [MI(IL),  BurstCompile] public static float smax_exp(this float t, float a, float b) {
-            var o = (f2(a - b, b - a) / t).exp();
+            float2 o = (f2(a - b, b - a) / t).exp();
             return f2(a, b).dot(o) / o.csum();
         }
         public static float2 smax_exp2(this float2 t, float2 a, float2 b) => f2(t.x.smax_exp(a.x, b.x), t.y.smax_exp(a.y, b.y));
